1c579be6807a72c3f6c912563a1113b141666c87
Sebastian Reimers <hallo@studio-link.de>
2018-01-17 21:44:01 +0100

    add mingw-w64-configure and mingw-w64-pkg-config

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash -ex
 
-function compile {
+compile() {
     pushd $1
     makepkg --skippgpcheck
     yes | LANG=C sudo pacman -U *.pkg.tar.xz
     popd
 }
 
-#pacman -S ppl zlib libmpc gmp --noconfirm
+main() {
+    compile mingw-w64-binutils
+    compile mingw-w64-headers
+    compile mingw-w64-headers-bootstrap
+    compile isl
+    compile osl
+    compile cloog
+    compile mingw-w64-gcc-base
+    compile mingw-w64-crt
+    compile mingw-w64-winpthreads
+    compile mingw-w64-gcc
+    compile mingw-w64-pkg-config
+    compile mingw-w64-configure
+}
 
-compile mingw-w64-binutils
-compile mingw-w64-headers
-compile mingw-w64-headers-bootstrap
-compile isl
-compile osl
-compile cloog
-compile mingw-w64-gcc-base
-compile mingw-w64-crt
-compile mingw-w64-winpthreads
-compile mingw-w64-gcc
+main
